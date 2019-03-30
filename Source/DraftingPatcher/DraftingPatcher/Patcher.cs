9f7e63cb27516ce5d8e138450886349b0ff4430e
juanosarg <31105173+juanosarg@users.noreply.github.com>
2019-03-30 16:38:51 +0100

    v4.4

--- Source/DraftingPatcher/DraftingPatcher/Patcher.cs
+++ Source/DraftingPatcher/DraftingPatcher/Patcher.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
 using Verse.AI;
+using RimWorld.Planet;
 
 
 // So, let's comment this code, since it uses Harmony and has moderate complexity
@@ -603,10 +604,45 @@ namespace DraftingPatcher
 
 
 
+    /*This Harmony postfix tries to add an endgame event
+    * 
+    */
+    [HarmonyPatch(typeof(MapParent))]
+    [HarmonyPatch("RecalculateHibernatableIncidentTargets")]
+
+    static class MapParent_RecalculateHibernatableIncidentTargets_Patch
+    {
+        [HarmonyPostfix]
+
+        public static void AddEndParts(ref MapParent __instance)
+        {
+            HashSet<IncidentTargetTagDef> theListToChange;
+            theListToChange = (HashSet < IncidentTargetTagDef >)typeof(MapParent).GetField("hibernatableIncidentTargets", AccessTools.all).GetValue(__instance);
+            //typeof(MapParent).GetField("hibernatableIncidentTargets", AccessTools.all).SetValue(__instance,null);
+            //__instance.hibernatableIncidentTargets = null;
+            foreach (ThingWithComps current in __instance.Map.listerThings.ThingsOfDef(ThingDef.Named("GR_GameEnder")).OfType<ThingWithComps>())
+            {
+                CompHibernatable compHibernatable = current.TryGetComp<CompHibernatable>();
+                if (compHibernatable != null && compHibernatable.State == HibernatableStateDefOf.Starting && compHibernatable.Props.incidentTargetWhileStarting != null)
+                {
+                    if (theListToChange == null)
+                    {
+                        typeof(MapParent).GetField("hibernatableIncidentTargets", AccessTools.all).SetValue(__instance, new HashSet<IncidentTargetTagDef>());
+
+                       // this.hibernatableIncidentTargets = new HashSet<IncidentTargetTagDef>();
+                    }
+                    theListToChange = (HashSet<IncidentTargetTagDef>)typeof(MapParent).GetField("hibernatableIncidentTargets", AccessTools.all).GetValue(__instance);
+                    theListToChange.Add(compHibernatable.Props.incidentTargetWhileStarting);
+                    typeof(MapParent).GetField("hibernatableIncidentTargets", AccessTools.all).SetValue(__instance, theListToChange);
+
+                    // this.hibernatableIncidentTargets.Add(compHibernatable.Props.incidentTargetWhileStarting);
+                }
+            }
+        }
+    }
 
 
 
 
 
-
-}
+        }
