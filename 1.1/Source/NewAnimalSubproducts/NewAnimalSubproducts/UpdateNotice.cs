845789de0b13773f0b3349fbec82c36205846dba
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-05-23 11:02:13 +0200

    v5.09

---
+++ 1.1/Source/NewAnimalSubproducts/NewAnimalSubproducts/UpdateNotice.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using RimWorld.Planet;
+
+namespace NewAnimalSubproducts
+{
+    public class UpdateNotice : MapComponent
+    {
+
+        public bool sentUpdateLetter = false;
+       
+        public UpdateNotice(Map map) : base(map)
+        {
+
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look<bool>(ref this.sentUpdateLetter, "sentUpdateLetter", false, true);
+
+
+        }
+
+        public override void MapComponentTick()
+        {
+            base.MapComponentTick();
+            if (!sentUpdateLetter)
+            {
+                if (!ModLister.HasActiveModWithName("Vanilla Textures Expanded - Genetic Rim"))
+                {
+                    Find.LetterStack.ReceiveLetter("GR_UpdateLetterLabel".Translate(), "GR_UpdateLetterText".Translate(), DefDatabase<LetterDef>.GetNamed("GR_UpdateLetter"));
+                }
+                sentUpdateLetter = true;
+            }
+
+           
+        }
+
+
+    }
+
+
+}
+
